ff2c5d00246a123b961d92b9b96612ace99f151b
errerew <erlina31acc@gmail.com>
2026-05-13 03:18:54 +0700

    memperbaiki tampilan datagrid

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -177,7 +177,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(460, 141);
+            this.dataGridView1.Size = new System.Drawing.Size(485, 141);
             this.dataGridView1.TabIndex = 17;
             // 
             // dtpTanggalKetersediaan
