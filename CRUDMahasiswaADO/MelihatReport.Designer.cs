8ed973bb0487e1b1a791bfe1c63d0fdc69566be8
errerew <erlina31acc@gmail.com>
2026-05-13 03:39:19 +0700

    memperbaiki tampilan button cari pada form report

--- CRUDMahasiswaADO/MelihatReport.Designer.cs
+++ CRUDMahasiswaADO/MelihatReport.Designer.cs
@@ -39,20 +39,22 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(51, 69);
+            this.dataGridView1.Location = new System.Drawing.Point(38, 56);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(705, 276);
+            this.dataGridView1.Size = new System.Drawing.Size(529, 224);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnRefresh
             // 
             this.btnRefresh.BackColor = System.Drawing.Color.LightGreen;
-            this.btnRefresh.Location = new System.Drawing.Point(290, 367);
+            this.btnRefresh.Location = new System.Drawing.Point(218, 298);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(115, 39);
+            this.btnRefresh.Size = new System.Drawing.Size(86, 32);
             this.btnRefresh.TabIndex = 1;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = false;
@@ -61,9 +63,10 @@
             // btnClose
             // 
             this.btnClose.BackColor = System.Drawing.Color.Red;
-            this.btnClose.Location = new System.Drawing.Point(447, 367);
+            this.btnClose.Location = new System.Drawing.Point(335, 298);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(115, 39);
+            this.btnClose.Size = new System.Drawing.Size(86, 32);
             this.btnClose.TabIndex = 2;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -71,17 +74,19 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(406, 31);
+            this.txtSearch.Location = new System.Drawing.Point(304, 25);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(278, 22);
+            this.txtSearch.Size = new System.Drawing.Size(210, 20);
             this.txtSearch.TabIndex = 4;
             // 
             // btnCari
             // 
-            this.btnCari.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.btnCari.Location = new System.Drawing.Point(690, 31);
+            this.btnCari.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCari.Location = new System.Drawing.Point(518, 25);
+            this.btnCari.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCari.Name = "btnCari";
-            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.Size = new System.Drawing.Size(56, 19);
             this.btnCari.TabIndex = 5;
             this.btnCari.Text = "Cari";
             this.btnCari.UseVisualStyleBackColor = false;
@@ -89,14 +94,15 @@
             // 
             // MelihatReport
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.btnCari);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MelihatReport";
             this.Text = "MelihatReport";
             this.Load += new System.EventHandler(this.MelihatReport_Load_1);
